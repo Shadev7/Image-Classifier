522f7edf23d3a760cd12034d98d19f44b7c03080
Yang <zhy0216@gmail.com>
2016-03-27 02:03:56 -0400

    fix script

--- make-overfeat-cmd.py
+++ make-overfeat-cmd.py
@@ -16,7 +16,7 @@ def check_size(file_path):
 def resize(file_path):
     cmd = "convert %s -resize 231x231 %s"%(file_path, file_path)
     print cmd
-    
+
 for category in os.listdir(folder):
     test_path = os.path.join(folder, category)
     if os.path.isdir(test_path):
@@ -24,6 +24,8 @@ for category in os.listdir(folder):
             image_path = os.path.join(test_path, image)
             featurename = image_path+".features"
             des = "deep/%s-feature/%s"%(folder, featurename[5:])
+            new_folder = "deep/%s-feature/%s"%(folder,category)
+            os.system("mkdir -p %s"%new_folder)
             if not os.path.isfile(des):
                 width, height = check_size(image_path)
                 if  width < 231 and height < 231:
