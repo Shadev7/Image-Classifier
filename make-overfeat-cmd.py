fad76a7ea2b424d002be9ec2df73ded66b1f0b8c
Yang <zhy0216@gmail.com>
2016-03-27 01:43:01 -0400

    fix

--- make-overfeat-cmd.py
+++ make-overfeat-cmd.py
@@ -11,7 +11,7 @@ for category in os.listdir(folder):
             image_path = os.path.join(test_path, image)
             featurename = image_path+".features"
             if not os.path.isfile(featurename):
-                cmd = "./overfeat/bin/linux_64/overfeat_batch -f %s > deep/%s-feature/%s"%(image_path, folder, featurename)
+                cmd = "./overfeat/bin/linux_64/overfeat_batch -f %s > deep/%s-feature/%s"%(image_path, folder, featurename[5:])
                 print "cmd:%s"%cmd
                 os.system(cmd)
 
