90705f99282c061f77f68fc76edec6f4cc91f56c
Yang <zhy0216@gmail.com>
2016-03-27 01:59:48 -0400

    improve

--- make-overfeat-cmd.py
+++ make-overfeat-cmd.py
@@ -2,16 +2,33 @@
 
 
 import os
+import subprocess
 folder = "test"
 
+
+## http://stackoverflow.com/a/10759761/847357
+def check_size(file_path):
+    path = file_path
+    dim = subprocess.Popen(["identify","-format","\"%w,%h\"",path], stdout=subprocess.PIPE).communicate()[0]
+    (width, height) = [ int(x) for x in re.sub('[\t\r\n"]', '', dim).split(',') ]
+    return (width, height)
+
+def resize(file_path):
+    cmd = "convert %s -resize 231x231 %s"%(file_path, file_path)
+    print cmd
+    
 for category in os.listdir(folder):
     test_path = os.path.join(folder, category)
     if os.path.isdir(test_path):
         for image in os.listdir(test_path):
             image_path = os.path.join(test_path, image)
             featurename = image_path+".features"
-            if not os.path.isfile(featurename):
-                cmd = "./overfeat/bin/linux_64/overfeat_batch -f %s > deep/%s-feature/%s"%(image_path, folder, featurename[5:])
+            des = "deep/%s-feature/%s"%(folder, featurename[5:])
+            if not os.path.isfile(des):
+                width, height = check_size(image_path)
+                if  width < 231 and height < 231:
+                    resize(image_path)
+                cmd = "./overfeat/bin/linux_64/overfeat -f %s > %s"%(image_path, des)
                 print "cmd:%s"%cmd
                 os.system(cmd)
 
